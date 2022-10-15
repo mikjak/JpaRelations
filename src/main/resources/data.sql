c310cac125e50114568f7ac06e5130b1daf24864
Jakub Mikuła <jakub.mikula@lhsystems.com>
2022-10-15 23:37:13 +0200

    Initialized db with the data.sql file.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (description) VALUES ('Poland');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Kebab');
+INSERT INTO category (description) VALUES ('Pizza');
+INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pinch');
+INSERT INTO unit_of_measure (description) VALUES ('Ounce');
